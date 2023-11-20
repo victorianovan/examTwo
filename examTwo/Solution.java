3be70279d800f66339e8a3841f93d81f909535e5
victorianovan <143017859+victorianovan@users.noreply.github.com>
2023-11-20 12:52:10 -0600

    Update Solution.java
    ,

--- examTwo/Solution.java
+++ examTwo/Solution.java
@@ -27,11 +27,66 @@ public class Solution{
 	}
 	
 	public static int[] oddEvens(String str) {
-	//** Implement this method according to the instruction and examples on
-	//** the repo README.md file.
+		int index;
+		int index2 = 0;
+		char ch;
+		char ch2 = 0;
+		char ch3 = 0;
+		int[] array;
+		int count = 0;
+		
+		for(index=0;index<str.length();index++){
+			ch = str.charAt(index);
+			if (index + 1 < str.length()) {
+				ch2 = str.charAt(index + 1);
+			}
+			if (index + 2 < str.length()) {
+				ch3 = str.charAt(index + 2);
+			}
 
-		return null;
-	}
+			if (ch >= '0' && ch <= '9') { 
+				if (ch2 == 43 || ch2 == 45) {
+					if (ch3 >='0' && ch3 <= '9') {
+						count--;
+					}
+				}
+				count++;
+			}
+		}
+		array=new int[count];
+		for(index=0;index<str.length();index++){
+			ch = str.charAt(index);
+			if(ch >= '0' && ch <= 57 ) {
+				if (ch2 == 43 || ch2 == 45) {
+					if (ch3 >='0' && ch3 <= '9') {
+						array[index2] = ch - '0';
+						index2++;
+					}
+					}
+			}
+		
+          if (index + 1 < str.length()) {
+				ch2 = str.charAt(index + 1);
+			}
+			if (index + 2 < str.length()) {
+				ch3 = str.charAt(index + 2);
+			}
+			if (ch >= 48 && ch <= 57) {
+				if (ch2 == 43) {
+					 array[index2] = (ch - '0') + (ch3 - '0');
+					 index += 3;
+				} else if (ch2 == 45) {
+					 array[index2] = (ch -'0') - (ch3 - '0');
+					 index += 3;
+				}
+
+			 index2++;
+			}
+		}                                                               
+		return array;}}
+	
+		
+
 	
 
-}
+
